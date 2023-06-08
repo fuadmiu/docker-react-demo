099cc79b611d7e101bdbb88c4fc259d4425aeb35
Fuad <mohammad.fuad@miu.edu>
2023-06-08 16:51:44 -0500

    Test changed, port exposed

--- src/dummy.test.js
+++ src/dummy.test.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+    const div = ReactDOM.createRoot(document.createElement('div'));
+    div.render(<App />);
 });
